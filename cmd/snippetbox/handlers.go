181ac3d1738bff9b69c0a1ee8714063adaa94c03
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-04-22 00:16:21 +0700

    Restructure project

--- cmd/snippetbox/handlers.go
+++ cmd/snippetbox/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -32,15 +31,4 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte("Create a specific snippet..."))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	mux.HandleFunc("/snippet/view/", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	log.Println("Starting server on :8000")
-	err := http.ListenAndServe(":8000", mux)
-	log.Fatal(err)
 }

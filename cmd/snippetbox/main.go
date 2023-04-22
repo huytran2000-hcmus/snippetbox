ee232d0663057f2eb3d633effc95d5b40d4c5c4c
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-04-22 15:58:59 +0700

    Add addr flag and logging

--- cmd/snippetbox/main.go
+++ cmd/snippetbox/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stdout, "ERROR\t", log.LstdFlags|log.Lshortfile)
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errLog.Fatal(err)
 }

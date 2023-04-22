0bf71b0dcc72a663cfbe3088d3b0bdd3c3f08b8c
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-04-22 13:48:12 +0700

    Add home page

--- cmd/snippetbox/handlers.go
+++ cmd/snippetbox/handlers.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from snippetbox\n"))
+
+	files := []string{
+		"./ui/html/base.gohtml",
+		"./ui/html/pages/home.gohtml",
+		"./ui/html/partials/nav.gohtml",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
--- cmd/snippetbox/main.go
+++ cmd/snippetbox/main.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.HandleFunc("/", hello)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :8000")
 	err := http.ListenAndServe(":8000", mux)

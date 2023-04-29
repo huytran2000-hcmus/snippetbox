3fa4137e8c187c59d6f014deca29938d06ac7f10
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-04-30 01:40:44 +0700

    Add log requests middleware for all

--- cmd/snippetbox/middlewares.go
+++ cmd/snippetbox/middlewares.go
@@ -2,6 +2,14 @@ package main
 
 import "net/http"
 
+func (app *Application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com; frame-ancestors 'none'")

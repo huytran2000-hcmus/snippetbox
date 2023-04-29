fed5623606a618c9aec9a2567ed3e9fa4338401d
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-04-30 01:42:57 +0700

    Add recover from panic middleware for all

--- cmd/snippetbox/middlewares.go
+++ cmd/snippetbox/middlewares.go
@@ -1,10 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *Application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *Application) recoverFromPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
 
 		next.ServeHTTP(w, r)
 	})

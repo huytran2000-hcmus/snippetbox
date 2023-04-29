b1bcae8ec35f5d6b37723091a63b2b9f6a1cb56e
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-04-30 01:39:19 +0700

    Add secure headers for all responses

--- cmd/snippetbox/routes.go
+++ cmd/snippetbox/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *Application) routes() *http.ServeMux {
+func (app *Application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -12,5 +12,5 @@ func (app *Application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return secureHeaders(mux)
 }
